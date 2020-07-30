d4eed52210c1e05c516aee81915500dff65d9ccc
JP-Go <jpedro1580@gmail.com>
2020-07-29 21:23:38 -0300

    Efeito de troca de slides na home

--- scripts/slideBehavior.js
+++ scripts/slideBehavior.js
@@ -1,11 +1,11 @@
 // Atribui à variavies correnspondentes a lista de slides e um indice de slides
 
-let slideList = document.querySelectorAll('.slides__image');
+let slideList = document.querySelectorAll(".slides__image");
 let SlideIndex = 0;
 
 // Mostra o primeiro slide
 showSlide(SlideIndex);
-
+setInterval(changeIndex, 5000, 1);
 // Modifica o contador do slide por um fator number
 function changeIndex(number) {
   if (number >= 0) {
@@ -28,8 +28,15 @@ function showSlide(index) {
   let slide = slideList[index];
   // Faz cada slide dessaparecer antes de carregar a página;
   slideList.forEach((element) => {
-    element.style.display = 'none';
+    element.style.display = "none";
   });
   // Mostra o slide atual
-  slide.style.display = 'block';
+  slide.style.display = "block";
+  // Mostra a legenda da imagem de acordo com o índice
+  let legendas = {
+    0: "Uma Biblioteca",
+    1: "Pôr do Sol",
+    2: "Foto de longa exposição a noite em uma estrada",
+  };
+  document.getElementById("legenda").innerText = legendas[`${index}`];
 }
